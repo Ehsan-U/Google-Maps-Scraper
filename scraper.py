3bf7a6793f85b20cce9e78225a9e61e9e1903a65
EhsanUllah <88642086+Ehsan-U@users.noreply.github.com>
2022-12-25 12:16:52 +0500

    playwright version

--- scraper.py
+++ scraper.py
@@ -11,28 +11,38 @@ class Google():
         self.url = url
         self.limit = limit
         self.play = sync_playwright().start()
-        self.browser = self.play.chromium.launch(headless=False, slow_mo=50)
+        self.browser = self.play.chromium.launch(headless=True, slow_mo=50)
         self.page = self.browser.new_page()
-        self.page.set_viewport_size({"width":1280,"height":720})
+        # self.page.set_viewport_size({"width":1280,"height":720})
         self.records = 0
         self.con = Console()
         self.all_items = []
-
-
+# background-image:url('https://lh5.googleusercontent.com/p/AF1QipOO23o7EIe6JKxQsBwj33I68ECnT8WCruB4DsSk=w300-h225-p-k-no');width:calc(50% - 1px);padding-top:calc(37.5% - 1px)
     def initiate(self):
+        # self.page.route("**googleusercontent.com/p/*w300-h225*", lambda route: route.abort()
+        #    if route.request.resource_type == "image"
+        #    else route.continue_()
+        #   )
         self.page.goto(self.url)
         while self.records < self.limit:
             goto = self.page.locator("//div[@data-review-id and @jsaction]/div[3]").last
             names = self.page.locator("//div[@data-review-id and @jsaction]/div[3]/div[@jsan]/div[@style]/div[1]/a/div[1]/span")
             reviews = self.page.locator("//div[@data-review-id and @jsaction]/div[3]/div[last()]/div[@id]/span[@class]")
+            dates = self.page.locator("//div[@data-review-id and @jsaction]/div[3]/div[last()]/div[1]/span[3]")
             items = []
-            for name,review in zip(names.element_handles()[self.records:], reviews.element_handles()[self.records:]):
-                self.records +=1
+            for name,review,datte in zip(names.element_handles()[self.records:], reviews.element_handles()[self.records:], dates.element_handles()[self.records:]):
+                self.records += 1
+                # print(f"\r{self.records}",end='')
                 item = [
                     unidecode(name.text_content()),
-                    unidecode(review.text_content())
+                    unidecode(review.text_content()),
+                    datte.text_content()
                 ]
                 items.append(item)
+                if self.records == self.limit:
+                    break
+                else:
+                    continue
             yield items
             goto.scroll_into_view_if_needed()
             sleep(10)
@@ -40,13 +50,14 @@ class Google():
     def main(self):
         try:
             for items in g.initiate():
-                self.all_items += self.all_items + items
+                for item in items:
+                    self.all_items.append(item)
         except Exception:
             pass
         finally:
             with open("data.csv", 'w') as f:
                 writer = csv.writer(f)
-                writer.writerow(['Name', 'Review'])
+                writer.writerow(['Name', 'Review', 'Date'])
                 for item in self.all_items:
                     writer.writerow(item)
             f.close()
